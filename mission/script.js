e44a909875079689fd999b030b727621fe377bb4
Dallin Sprague <61210689+DSprague12@users.noreply.github.com>
2026-09-21 11:38:47 -0600

    Basic dark mode and fixed fonts

---
+++ mission/script.js
@@ -0,0 +1,17 @@
+let selectElem = document.querySelector('select');
+let body = document.querySelector('body');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+}           
+                    
