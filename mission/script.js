b018bc1b0249fcb1f507af1b31084c5551209c5e
Dallin Sprague <61210689+DSprague12@users.noreply.github.com>
2026-09-22 12:54:57 -0600

    Dark mode completed

--- mission/script.js
+++ mission/script.js
@@ -7,11 +7,13 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        body.style.backgroundColor = "black";
+        body.style.backgroundColor = "#333";
         body.style.color = "white";
+        logo.src = "byui-logo-white.png";
     } else {
         body.style.backgroundColor = "white";
         body.style.color = "black";
+        logo.src = "byui-logo-blue.webp";
     }
 }           
                     
